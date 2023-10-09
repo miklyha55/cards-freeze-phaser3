176d95c174aaa581e751a4aa738ed947b6df3c87
Roman Kolenko <miklyha55@yandex.ru>
2023-10-09 12:02:16 +0300

    refactor(template) style, interface

--- src/components/rect/types/index.ts
+++
@@ -1,5 +0,0 @@
-import { IROComponentCfg } from "../../core/types";
-
-export interface IROSpriteCfg extends IROComponentCfg {
-    readonly texture: string;
-}
--- src/components/resize/types/index.ts
+++ src/components/resize/types/index.ts
@@ -2,15 +2,15 @@ import { ISize, IVec2 } from "../../../utils/types";
 import { IROComponentCfg } from "../../core/types";
 
 export interface IROResizeCfg extends IROComponentCfg {
-    landscape: IOrientationCfg;
-    portrait: IOrientationCfg;
-    parent?: Phaser.GameObjects.Container | Phaser.GameObjects.Sprite | Phaser.GameObjects.Text | Phaser.GameObjects.TileSprite,
+    readonly landscape: IOrientationCfg;
+    readonly portrait: IOrientationCfg;
+    readonly parent?: Phaser.GameObjects.Container | Phaser.GameObjects.Sprite | Phaser.GameObjects.Text | Phaser.GameObjects.TileSprite,
 }
 
 export interface IOrientationCfg {
-    relativePosition?: IVec2;
-    absolutePosition?: IVec2;
-    scale?: IVec2;
-    size?: ISize;
-    origin?: IVec2;
+    readonly relativePosition?: IVec2;
+    readonly absolutePosition?: IVec2;
+    readonly scale?: IVec2;
+    readonly size?: ISize;
+    readonly origin?: IVec2;
 }
